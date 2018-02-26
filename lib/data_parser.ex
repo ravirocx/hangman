14a82b2e96a6af4e6f87e3605646ea693d9500da
Ravi Raj <itsyourraviraj@gmail.com>
2018-02-26 17:25:59 +0530

    Optimised Data_parser

--- lib/data_parser.ex
+++ lib/data_parser.ex
@@ -1,11 +1,18 @@
 defmodule DataParser do
+
+	@loc "C:\\Users\\Ravi Raj\\Desktop\\Intern\\hangman\\words.txt"
+
 	def parse_data() do
 		##will parse data
-		list = ["TAVOR", "INSAS", "GLOCK", "CARBINE", "GALIL", "KALASHNIKOVA", "DRAGUNAV", "CHETAK", "TEJAS", "SUKHOI", "ARJUN", "RAFALE", "VIKRAMADTIYA", "STALLION"]
-    Enum.random(list)
-    |> String.graphemes
-    
+		{result, words} = File.read(@loc) 
+		case result do
+		   :error -> raise ArgumentError
+		   :ok -> 
+		   		words_list = String.split(words, "\r\n", trim: true)
+		    	Enum.random(words_list)
+		    	|>String.graphemes
+		end
+  
 	end
 end
 
-#IO.puts DataParser.parse_data()
--- test/hangman_test.exs
+++ test/hangman_test.exs
@@ -2,7 +2,15 @@ defmodule HangmanTest do
   use ExUnit.Case
   doctest Hangman
 
-  test "Checking Supervisor" do
-    assert {:ok, _}==Hangman.start() 
+  test "Checking Hangman's start Function" do
+    assert Hangman.start() == {:ok, _}
   end
+
+  #test "Checking Main's start Function" do
+  #  assert {:ok, _} == Main.start_link() 
+  #end
+
+  #test "Checking Super's start Function" do
+  #  assert Super.start_link() == {:ok, _}
+  #end
 end
